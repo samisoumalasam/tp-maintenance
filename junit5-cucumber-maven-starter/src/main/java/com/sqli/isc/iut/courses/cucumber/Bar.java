0df3c0cb773b53b79b44df51333c725a336ac005
Pascal Simon <simon.pascal@etu.u-bordeaux.fr>
2025-05-22 12:08:09 +0200

    classe person et bar terminé

--- junit5-cucumber-maven-starter/src/main/java/com/sqli/isc/iut/courses/cucumber/Bar.java
+++ junit5-cucumber-maven-starter/src/main/java/com/sqli/isc/iut/courses/cucumber/Bar.java
@@ -5,11 +5,34 @@ import java.util.HashMap;
 public class Bar {
     static int NBMAXPEOPLE = 10;
     private HashMap<Person, Integer> PeopleBill = new HashMap<>();
+    private Person[] people;
     private int count;
+    private int totalBill = 0;
+
+    public String getState(){
+        String response ="Bar is open\n";
+
+        if (count == 0) {
+            response += "No one in the bar\n";
+        } else {
+            if(count == 10){
+                response += "Bar is full\n";
+            } else {
+                response += "People in the bar:\n";
+                for (int i = 0; i < count; i++) {
+                    response += people[i].getName() + "\n";
+                }
+            }
+        }
+        return response;
+    }
+
+
     public Bar() {
         this.people = new Person[NBMAXPEOPLE];
         this.count = 0;
     }
+
     public void addPerson(Person person) {
         if (count < NBMAXPEOPLE) {
             people[count] = person;
@@ -28,6 +51,16 @@ public class Bar {
             }
         }
     }
+    public boolean isInBar(Person person) {
+        boolean found = false;
+        for (int i = 0; i < count; i++) {
+            if (people[i].getName().equals(person.getName())) {
+                found = true;
+                break;
+            }
+        }
+        return found;
+    }
     public int getCount() {
         return count;
     }
@@ -37,5 +70,45 @@ public class Bar {
     public void setPeople(Person[] people) {
         this.people = people;
     }
+    public void getCocktailOfTheMonth(Person person) {
+        System.out.println("Cocktail of the month: Mojito");
+        addBill(person, 10);
+        totalBill += 10;
+    }
+    public void addBill(Person person, int bill) {
+        if (PeopleBill.containsKey(person)) {
+            PeopleBill.put(person, PeopleBill.get(person) + bill);
+        } else {
+            PeopleBill.put(person, bill);
+        }
+    }
+    public int getBill(Person person) {
+        if (PeopleBill.containsKey(person)) {
+            return PeopleBill.get(person);
+        } else {
+            return 0;
+        }
+    }
+    public void checkOutTheBill(Person person) {
+        if (PeopleBill.containsKey(person)) {
+            System.out.println("Bill for " + person.getName() + ": " + PeopleBill.get(person));
+            PeopleBill.remove(person);
+            totalBill -= PeopleBill.get(person);
+        } else {
+            System.out.println("No bill for " + person.getName());
+        }
+    }
+    public void exitBar(Person person) {
+        if(isInBar(person)) {
+            if(getBill(person) > 0) {
+                System.out.println(person.getName() + " has a bill of " + getBill(person));
+            } else {
+                removePerson(person);
+                System.out.println(person.getName() + " has exited the bar");
+            }
+        } else {
+            System.out.println(person.getName() + " is not in the bar");
+        }
 
+    }
 }
--- junit5-cucumber-maven-starter/src/main/java/com/sqli/isc/iut/courses/cucumber/Person.java
+++ junit5-cucumber-maven-starter/src/main/java/com/sqli/isc/iut/courses/cucumber/Person.java
@@ -7,7 +7,12 @@ import com.sqli.isc.iut.courses.cucumber.Emotion;
 public class Person {
     private String name;
     private Emotion currentEmotion;
-
+    private Bar bar;
+    public Person(String name, Bar bar) {
+        this.name = name;
+        this.currentEmotion = Emotion.CALM;
+        this.bar = bar;
+    }
     public Person(String name) {
         this.name = name;
         this.currentEmotion = Emotion.CALM;
@@ -20,6 +25,10 @@ public class Person {
     }
     public Emotion getCurrentEmotion() {
         return currentEmotion;
+    }
+    public void getADrink(){
+        System.out.println("I want a drink");
+        bar.getCocktailOfTheMonth(this);
     }
 };
 
